f135e7c374fdbcb484db203e94407b8401a70865
Masui Masanori <masanori840816@gmail.com>
2022-04-25 21:32:58 +0900

    add main page

--- main.go
+++ main.go
@@ -1,9 +1,33 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"path/filepath"
+	"sync"
 )
 
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	// "sync.Once" executes only one time.
+	t.once.Do(func() {
+		// "Must()" wraps "ParseFiles()" results, so I can put it into "templateHandler.templ" directly
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+	})
+	t.templ.Execute(w, "Hello world")
+}
+
 func main() {
-	fmt.Printf("hello")
+	http.Handle("/css/", http.FileServer(http.Dir("templates")))
+	http.Handle("/js/", http.FileServer(http.Dir("templates")))
+	// In this sample, "ServeHTTP()" is called twice.
+	// The second time is for loading "favicon.ico"
+	http.Handle("/", &templateHandler{filename: "index.html"})
+	log.Fatal(http.ListenAndServe("localhost:8080", nil))
 }
